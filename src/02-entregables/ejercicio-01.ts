5b3af2ad90576c40d01b955e0422d1324781eb37
zumaichi <dzumaquero@gmail.com>
2025-07-09 12:11:05 +0200

    1.2

--- src/02-entregables/ejercicio-01.ts
+++ src/02-entregables/ejercicio-01.ts
@@ -5,5 +5,15 @@ const head = (array: number[]) => {
   const [first] = array;
   return first;
 };
-
+console.log("mostrar el primer elemento del array(destrucuturing)");
 console.log(head(array));
+
+const tail = (array: number[]) => {
+  const [, ...rest] = array;
+  return rest;
+};
+console.log("mostrar el todos del array menos el primero(rest)");
+console.log(tail(array));
+
+const init = (array: number[]) => {};
+console.log("mostrar los todos del array menos el último");
