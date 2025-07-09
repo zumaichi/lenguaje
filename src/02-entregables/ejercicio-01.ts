16fae3dcf21073ba2eecdeb545d0b9f60b47e0c6
zumaichi <dzumaquero@gmail.com>
2025-07-09 11:43:01 +0200

    2

--- src/02-entregables/ejercicio-01.ts
+++ src/02-entregables/ejercicio-01.ts
@@ -1,8 +1,9 @@
 console.log("************** DELIVERABLE 01 *********************");
-const head = (array) => {
+let array = [1, 2, 3, 4, 5];
+
+const head = (array: number[]) => {
   const [first] = array;
   return first;
 };
 
-let array = [1, 2, 3, 4, 5];
 console.log(head(array));
--- src/02-entregables/ejercicio-02.ts
+++ src/02-entregables/ejercicio-02.ts
@@ -1,1 +1,10 @@
 console.log("************** DELIVERABLE 02 *********************");
+let array = [1, 2, 3, 4, 5];
+
+const tail = (array: number[]) => {
+  const [, ...rest] = array;
+  return rest;
+};
+
+console.log(tail(array));
+ 
