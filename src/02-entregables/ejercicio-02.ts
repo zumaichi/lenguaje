829a07b53e309528194b0dfd91998fb3b81beed0
zumaichi <dzumaquero@gmail.com>
2025-07-09 13:49:04 +0200

    2.1

--- src/02-entregables/ejercicio-02.ts
+++ src/02-entregables/ejercicio-02.ts
@@ -1,4 +1,8 @@
 console.log("************** DELIVERABLE 02 *********************");
-
-
- 
+const a = [1, 2, 3];
+const b = [4, 5, 6];
+const concat = (a: number[], b: number[]) => {
+  return [...a, ...b];
+};
+console.log(concat(a, b));
+//opcional
