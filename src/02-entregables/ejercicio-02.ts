ea0363503a23103d859724cd4d2c7659b3002b47
zumaichi <dzumaquero@gmail.com>
2025-07-09 16:12:37 +0200

    2.2

--- src/02-entregables/ejercicio-02.ts
+++ src/02-entregables/ejercicio-02.ts
@@ -1,8 +1,23 @@
 console.log("************** DELIVERABLE 02 *********************");
 const a = [1, 2, 3];
 const b = [4, 5, 6];
+const c = [7, 8, 9];
 const concat = (a: number[], b: number[]) => {
   return [...a, ...b];
 };
 console.log(concat(a, b));
 //opcional
+const concatalternative = (a: number[], b: number[], c: number[]) => {
+  let result = [];
+  for (let i = 0; i < a.length; i++) {
+    result.push(a[i]);
+  }
+  for (let i = 0; i < b.length; i++) {
+    result.push(b[i]);
+  }
+  for (let i = 0; i < c.length; i++) {
+    result.push(c[i]);
+  }
+  return result;
+};
+console.log(concatalternative(a, b, c));
