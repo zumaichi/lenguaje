477289e9816c6bf17f47e77216e09a1fc17ca6af
zumaichi <dzumaquero@gmail.com>
2025-07-09 10:18:42 +0200

    first commit

---
+++ src/02-entregables/ejercicio-01.ts
@@ -0,0 +1,8 @@
+console.log("************** DELIVERABLE 01 *********************");
+const head = (array) => {
+  const [first] = array;
+  return first;
+};
+
+let array = [1, 2, 3, 4, 5];
+console.log(head(array));
