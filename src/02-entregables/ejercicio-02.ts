b94b3f0fd6a035a601ef9f55fb1738473e5a78db
zumaichi <dzumaquero@gmail.com>
2025-07-12 16:12:43 +0200

    2 correccion

--- src/02-entregables/ejercicio-02.ts
+++ src/02-entregables/ejercicio-02.ts
@@ -2,24 +2,22 @@ console.log("************** DELIVERABLE 02 *********************");
 const a = [1, 2, 3];
 const b = [4, 5, 6];
 const c = [7, 8, 9];
+const d = [10, 11, 12];
+
 const concat = (a: number[], b: number[]) => {
   return [...a, ...b];
 };
 console.log("Concatenar dos arrays");
 console.log(concat(a, b));
-//opcional
-const concatAlternative = (a: number[], b: number[], c: number[]) => {
-  let Concatenar = [];
-  for (let i = 0; i < a.length; i++) {
-    Concatenar.push(a[i]);
-  }
-  for (let i = 0; i < b.length; i++) {
-    Concatenar.push(b[i]);
-  }
-  for (let i = 0; i < c.length; i++) {
-    Concatenar.push(c[i]);
+
+const concatMultiple = (...arrays: number[][]) => {
+  let Concatenar: number[] = [];
+  for (let i = 0; i < arrays.length; i++) {
+    for (let j = 0; j < arrays[i].length; j++) {
+      Concatenar.push(arrays[i][j]);
+    }
   }
   return Concatenar;
 };
-console.log("Concatenar tres arrays de forma alternativa");
-console.log(concatAlternative(a, b, c));
+console.log("Concatenar arrays de indistintamente el numero de arrays");
+console.log(concatMultiple(a, b, c,d));
